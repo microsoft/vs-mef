ecff31092ea42e9d5ba56468134f84afa8387f0e
Andrew Arnott <andrewarnott@gmail.com>
2013-07-04 08:59:47 -0400

    Adds container isolation test.

---
+++ Microsoft.VisualStudio.Composition.Tests/ContainerIsolationTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ContainerIsolationTests
+    {
+        [Fact]
+        public void TwoContainersDoNotShareAnyExports()
+        {
+            var configuration = CompositionConfiguration.Create(typeof(SharedExport));
+            var container1 = configuration.CreateContainer();
+            var container2 = configuration.CreateContainer();
+
+            var export1 = container1.GetExport<SharedExport>();
+            var export2 = container2.GetExport<SharedExport>();
+            Assert.NotSame(export1, export2);
+        }
+
+        [Export, Shared]
+        public class SharedExport { }
+    }
+}
