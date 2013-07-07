884045ed3d4891289c31f22c073a82490af39ff6
Andrew Arnott <andrewarnott@gmail.com>
2013-07-07 07:29:22 -0700

    Adds ToLazy extension method.
    We don't use it ourselves (yet anyway), since the runtime type checks it performs we can perform at configuration time and when that's possible we always prefer tha.

---
+++ Microsoft.VisualStudio.Composition/CompositionUtilities.cs
@@ -0,0 +1,37 @@
+﻿namespace Microsoft.VisualStudio.Composition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Validation;
+
+    public static class CompositionUtilities
+    {
+        public static Lazy<TBase> ToLazy<T, TBase>(this ILazy<T> lazy) where TBase : T
+        {
+            Requires.NotNull(lazy, "lazy");
+
+            // LazyPart<T> (which implements ILazy<T>) derives from Lazy<T>, so if
+            // T === TBase, a simple cast is sufficient.
+            var asCast = lazy as Lazy<TBase>;
+            if (asCast != null)
+            {
+                return asCast;
+            }
+
+            // Create a new lazy that wraps this one. Always make it thread-safe,
+            // not to protect the inner lazy but the outer one.
+            // But if the inner lazy is already evaluated, we'll force-evaluate the outer one
+            // to match, in which case we don't need thread safety since we're doing it here on just one thread.
+            var newLazy = new Lazy<TBase>(() => (TBase)lazy.Value, isThreadSafe: !lazy.IsValueCreated);
+            if (lazy.IsValueCreated)
+            {
+                var throwaway = newLazy.Value;
+            }
+
+            return newLazy;
+        }
+    }
+}
