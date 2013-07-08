38eccba68fad2dbbda3449888bd60326f3784c87
Andrew Arnott <andrewarnott@gmail.com>
2013-07-08 07:18:23 -0700

    Adds tests for exporting fields, properties and methods.

---
+++ Microsoft.VisualStudio.Composition.Tests/ExportingMembersTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    public class ExportingMembersTests
+    {
+        [MefFact(CompositionEngines.V1)]
+        public void ExportedField(IContainer container)
+        {
+            string actual = container.GetExportedValue<string>("Field");
+            Assert.Equal("Andrew", actual);
+        }
+
+        [MefFact(CompositionEngines.V1 | CompositionEngines.V2)]
+        public void ExportedProperty(IContainer container)
+        {
+            string actual = container.GetExportedValue<string>("Property");
+            Assert.Equal("Andrew", actual);
+        }
+
+        [MefFact(CompositionEngines.V1)]
+        public void ExportedMethod(IContainer container)
+        {
+            var actual = container.GetExportedValue<Action>("Method");
+            Assert.NotNull(actual);
+        }
+
+        public class ExportingMembersClass
+        {
+            [MefV1.Export("Field")]
+            public string Field = "Andrew";
+
+            [Export("Property")]
+            [MefV1.Export("Property")]
+            public string Property
+            {
+                get { return "Andrew"; }
+            }
+
+            [MefV1.Export("Method")]
+            public void SomeAction()
+            {
+            }
+        }
+    }
+}
