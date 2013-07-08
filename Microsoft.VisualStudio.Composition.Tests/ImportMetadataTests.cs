12aed1cf076999162c9f500ea4cf96219c79c743
Andrew Arnott <andrewarnott@gmail.com>
2013-07-07 20:08:01 -0700

    Adds metadata tests.

---
+++ Microsoft.VisualStudio.Composition.Tests/ImportMetadataTests.cs
@@ -0,0 +1,150 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    public class ImportMetadataTests
+    {
+        [MefFact(CompositionEngines.V2 | CompositionEngines.V1)]
+        public void ImportWithMetadataDictionary(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportingPartWithMetadataDictionary>();
+            Assert.NotNull(importingPart.ImportingProperty);
+            Assert.Equal("b", importingPart.ImportingProperty.Metadata["a"]);
+            Assert.False(importingPart.ImportingProperty.IsValueCreated);
+            Assert.IsType<PartWithExportMetadata>(importingPart.ImportingProperty.Value);
+        }
+
+        [MefFact(CompositionEngines.V2 | CompositionEngines.V1)]
+        public void ImportManyWithMetadataDictionary(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportManyPartWithMetadataDictionary>();
+            Assert.NotNull(importingPart.ImportingProperty);
+            Assert.Equal(1, importingPart.ImportingProperty.Count());
+            Assert.Equal("b", importingPart.ImportingProperty.Single().Metadata["a"]);
+            Assert.False(importingPart.ImportingProperty.Single().IsValueCreated);
+            Assert.IsType<PartWithExportMetadata>(importingPart.ImportingProperty.Single().Value);
+        }
+
+        [MefFact(CompositionEngines.V2 | CompositionEngines.V1)]
+        public void ImportWithMetadataClass(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportingPartWithMetadataClass>();
+            Assert.NotNull(importingPart.ImportingProperty);
+            Assert.Equal("b", importingPart.ImportingProperty.Metadata.a);
+            Assert.False(importingPart.ImportingProperty.IsValueCreated);
+            Assert.IsType<PartWithExportMetadata>(importingPart.ImportingProperty.Value);
+        }
+
+        [MefFact(CompositionEngines.V2 | CompositionEngines.V1)]
+        public void ImportManyWithMetadataClass(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportManyPartWithMetadataClass>();
+            Assert.NotNull(importingPart.ImportingProperty);
+            Assert.Equal(1, importingPart.ImportingProperty.Count());
+            Assert.Equal("b", importingPart.ImportingProperty.Single().Metadata.a);
+            Assert.False(importingPart.ImportingProperty.Single().IsValueCreated);
+            Assert.IsType<PartWithExportMetadata>(importingPart.ImportingProperty.Single().Value);
+        }
+
+        [MefFact(CompositionEngines.V1)]
+        public void ImportWithMetadataInterface(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportingPartWithMetadataInterface>();
+            Assert.NotNull(importingPart.ImportingProperty);
+            Assert.Equal("b", importingPart.ImportingProperty.Metadata.a);
+            Assert.False(importingPart.ImportingProperty.IsValueCreated);
+            Assert.IsType<PartWithExportMetadata>(importingPart.ImportingProperty.Value);
+        }
+
+        [MefFact(CompositionEngines.V1)]
+        public void ImportManyWithMetadataInterface(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportManyPartWithMetadataInterface>();
+            Assert.NotNull(importingPart.ImportingProperty);
+            Assert.Equal(1, importingPart.ImportingProperty.Count());
+            Assert.Equal("b", importingPart.ImportingProperty.Single().Metadata.a);
+            Assert.False(importingPart.ImportingProperty.Single().IsValueCreated);
+            Assert.IsType<PartWithExportMetadata>(importingPart.ImportingProperty.Single().Value);
+        }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [MefV1.ExportMetadata("a", "b")]
+        [Export]
+        [ExportMetadata("a", "b")]
+        public class PartWithExportMetadata { }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [Export]
+        public class ImportingPartWithMetadataDictionary
+        {
+            [Import, MefV1.Import]
+            public Lazy<PartWithExportMetadata, IDictionary<string, object>> ImportingProperty { get; set; }
+        }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [Export]
+        public class ImportingPartWithMetadataInterface
+        {
+            [Import, MefV1.Import]
+            public Lazy<PartWithExportMetadata, IMetadata> ImportingProperty { get; set; }
+        }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [Export]
+        public class ImportingPartWithMetadataClass
+        {
+            [Import, MefV1.Import]
+            public Lazy<PartWithExportMetadata, MetadataClass> ImportingProperty { get; set; }
+        }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [Export]
+        public class ImportManyPartWithMetadataDictionary
+        {
+            [ImportMany, MefV1.ImportMany]
+            public IEnumerable<Lazy<PartWithExportMetadata, IDictionary<string, object>>> ImportingProperty { get; set; }
+        }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [Export]
+        public class ImportManyPartWithMetadataInterface
+        {
+            [ImportMany, MefV1.ImportMany]
+            public IEnumerable<Lazy<PartWithExportMetadata, IMetadata>> ImportingProperty { get; set; }
+        }
+
+        [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        [Export]
+        public class ImportManyPartWithMetadataClass
+        {
+            [ImportMany, MefV1.ImportMany]
+            public IEnumerable<Lazy<PartWithExportMetadata, MetadataClass>> ImportingProperty { get; set; }
+        }
+
+        public interface IMetadata
+        {
+            string a { get; }
+        }
+
+        public class MetadataClass
+        {
+            public MetadataClass(IDictionary<string, object> data)
+            {
+                object value;
+                if (data.TryGetValue("a", out value))
+                {
+                    this.a = (string)value;
+                }
+            }
+
+            public string a { get; set; }
+        }
+    }
+}
