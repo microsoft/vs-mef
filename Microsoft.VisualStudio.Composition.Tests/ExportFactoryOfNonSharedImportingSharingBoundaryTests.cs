e0a22346c5652b0d6d487720c5c4cba6e1e364ae
Andrew Arnott <andrewarnott@gmail.com>
2013-07-22 05:21:42 -0700

    Adds more sharing boundary tests.

---
+++ Microsoft.VisualStudio.Composition.Tests/ExportFactoryOfNonSharedImportingSharingBoundaryTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class ExportFactoryOfNonSharedImportingSharingBoundaryTests
+    {
+        [MefFact(CompositionEngines.V2)]
+        public void ExportFactoryOfNonSharedImportingSharingBoundary(IContainer container)
+        {
+            var factoryPart = container.GetExportedValue<RootPart>();
+            var nonSharedPart1 = factoryPart.Factory.CreateExport().Value;
+            var nonSharedPart2 = factoryPart.Factory.CreateExport().Value;
+            Assert.NotSame(nonSharedPart1.SharingBoundaryImport, nonSharedPart2.SharingBoundaryImport);
+        }
+
+        [Export, Shared("SharingBoundary")]
+        public class SharingBoundaryPart { }
+
+        [Export]
+        public class NonSharedBoundaryImportingPart
+        {
+            [Import]
+            public SharingBoundaryPart SharingBoundaryImport { get; set; }
+        }
+
+        [Export]
+        public class RootPart
+        {
+            [Import, SharingBoundary("SharingBoundary")]
+            public ExportFactory<NonSharedBoundaryImportingPart> Factory { get; set; }
+        }
+    }
+}
