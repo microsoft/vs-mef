3181d1197e9200185b44149bf925acc3a7cbc772
Andrew Arnott <andrewarnott@gmail.com>
2013-07-27 06:12:04 -0700

    Adds editor hosting test.
    Doesn't yet pass.

---
+++ Microsoft.VisualStudio.Composition.Tests/EditorHostTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Xunit;
+
+    public class EditorHostTests
+    {
+        private const string EditorAssemblyNames = @"
+            Microsoft.VisualStudio.Platform.VSEditor, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Text.Logic, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Text.UI, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Text.UI.Wpf, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+            Microsoft.VisualStudio.Language.StandardClassification, Version=12.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a
+        ";
+
+        [MefFact(CompositionEngines.V1, EditorAssemblyNames, Skip = "Not yet passing")]
+        public void ComposeEditor(IContainer container)
+        {
+            var editorFactory = container.GetExportedValue<ITextEditorFactoryService>();
+        }
+    }
+}
