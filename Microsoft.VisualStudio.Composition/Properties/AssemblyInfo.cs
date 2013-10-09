c59a80aefc14e28bcf40d07f87ce101f83182962
Andrew Arnott <andarno@microsoft.com>
2013-10-09 13:36:49 -0700

    Adds assembly description.

--- Microsoft.VisualStudio.Composition/Properties/AssemblyInfo.cs
+++ Microsoft.VisualStudio.Composition/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Microsoft.VisualStudio.Composition")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Lightening fast MEF engine, supporting System.ComponentModel.Composition and System.Composition.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Microsoft.VisualStudio.Composition")]
