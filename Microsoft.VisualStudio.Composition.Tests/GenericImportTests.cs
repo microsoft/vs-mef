2cbaf6bc0fc3a2d663435cd3fe5aefdcbf2aa9a1
Andrew Arnott <andarno@microsoft.com>
2014-04-12 01:14:51 -0700

    Adds a test for importing a generic type argument. V3 doesn't support it at present.

---
+++ Microsoft.VisualStudio.Composition.Tests/GenericImportTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    public class GenericImportTests
+    {
+        [MefFact(CompositionEngines.V1 | CompositionEngines.V2)]
+        public void GenericPartImportsTypeArgument(IContainer container)
+        {
+            var genericPart = container.GetExportedValue<PartThatImportsT<SomeOtherPart>>();
+            Assert.NotNull(genericPart);
+            Assert.IsType<SomeOtherPart>(genericPart.Value);
+        }
+
+        [Export, Shared, MefV1.Export]
+        public class PartThatImportsT<T>
+        {
+            [Import, MefV1.Import]
+            public T Value { get; set; }
+        }
+
+        [Export, Shared, MefV1.Export]
+        public class SomeOtherPart { }
+    }
+}
