8c17cea3a6e164d225181fd7f32c359e978d59ab
Andrew Arnott <andarno@microsoft.com>
2014-05-23 10:43:32 -0700

    Adds another failing test.

---
+++ Microsoft.VisualStudio.Composition.Tests/SharingBoundaryInvalidTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Composition.Hosting;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using CompositionFailedException = Microsoft.VisualStudio.Composition.CompositionFailedException;
+
+    public class SharingBoundaryInvalidTests
+    {
+        [MefFact(CompositionEngines.V2Compat, InvalidConfiguration = true, Skip = "Not yet passing")]
+        public void InvalidImportAcrossSharingBoundary(IContainer container)
+        {
+            var root = container.GetExportedValue<RootPart>();
+            var boundaryPartExport = root.Factory.CreateExport();
+        }
+
+        [Export]
+        public class RootPart
+        {
+            [Import, SharingBoundary("SomeBoundary")]
+            public ExportFactory<BoundaryPart> Factory { get; set; }
+        }
+
+        [Export, Shared("SomeBoundary")]
+        public class BoundaryPart
+        {
+            internal int DisposalCount { get; private set; }
+
+            [Import]
+            public PartThatImportsBoundaryPartFromOutsideBoundary BoundaryScopedSharedParts { get; set; }
+        }
+
+        [Export, Shared]
+        public class PartThatImportsBoundaryPartFromOutsideBoundary
+        {
+            [Import]
+            public BoundaryPart BoundaryPart { get; set; }
+        }
+    }
+}
