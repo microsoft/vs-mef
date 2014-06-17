c635ed36a9b90aba281e7ce6c2584bd39c3a9789
Andrew Arnott <andarno@microsoft.com>
2014-06-17 16:55:48 -0700

    Add a couple closed generic export part tests.

---
+++ Microsoft.VisualStudio.Composition.Tests/ClosedGenericExportedTypeTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    /// <summary>
+    /// MEFv1 and MEFv2 don't support closed generic type exports of MEF parts.
+    /// At the moment, MEFv3 supports it. If it becomes a problem however, we can surrender such support
+    /// if it means it makes something else better.
+    /// </summary>
+    [Trait("GenericExports", "Closed")]
+    public class ClosedGenericExportedTypeTests
+    {
+        [MefFact(CompositionEngines.V3EmulatingV1 | CompositionEngines.V3EmulatingV2)]
+        public void ImportOfClosedGenericExportedType(IContainer container)
+        {
+            var appleTree = container.GetExportedValue<Tree<Apple>>();
+            Assert.NotNull(appleTree);
+        }
+
+        [MefFact(CompositionEngines.V3EmulatingV1 | CompositionEngines.V3EmulatingV2)]
+        public void ImportOfClosedGenericExportedTypeWithWrongTypeArg(IContainer container)
+        {
+            Assert.Throws<CompositionFailedException>(() => container.GetExportedValue<Tree<Pear>>());
+        }
+
+        [Export(typeof(Tree<Apple>)), Shared]
+        [MefV1.Export(typeof(Tree<Apple>))]
+        public class Tree<T>
+        {
+            public List<T> Fruit { get; set; }
+        }
+
+        public class Apple { }
+
+        public class Pear { }
+    }
+}
