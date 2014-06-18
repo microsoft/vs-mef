ff0e1b4fcadf3bf5ecead7e5005017b05318fab8
Andrew Arnott <andarno@microsoft.com>
2014-06-18 10:25:33 -0700

    Adds constants class that may be useful in the future.

---
+++ Microsoft.VisualStudio.Composition/CompositionConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Microsoft.VisualStudio.Composition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class CompositionConstants
+    {
+        internal const string GenericContractMetadataName = CompositionNamespace + ".GenericContractName";
+        internal const string GenericParametersMetadataName = CompositionNamespace + ".GenericParameters";
+        internal const string ExportTypeIdentityMetadataName = "ExportTypeIdentity";
+
+        private const string CompositionNamespace = "System.ComponentModel.Composition";
+    }
+}
