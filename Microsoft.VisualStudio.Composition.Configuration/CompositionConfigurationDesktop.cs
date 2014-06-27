a662ddcd44c1981c221140962feaf0de68b66220
Andrew Arnott <andarno@microsoft.com>
2014-06-26 20:46:10 -0700

    Removes unused using directives.

--- Microsoft.VisualStudio.Composition.Configuration/CompositionConfigurationDesktop.cs
+++ Microsoft.VisualStudio.Composition.Configuration/CompositionConfigurationDesktop.cs
@@ -11,11 +11,6 @@
     using System.Threading;
     using System.Threading.Tasks;
     using System.Xml;
-    using Microsoft.Build.Construction;
-    using Microsoft.Build.Evaluation;
-    using Microsoft.Build.Execution;
-    using Microsoft.Build.Framework;
-    using Microsoft.Build.Logging;
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
