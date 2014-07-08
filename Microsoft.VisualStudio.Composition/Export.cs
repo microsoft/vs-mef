6c0e9ce442568901a014f28993266ed0e20d9d70
Andrew Arnott <andarno@microsoft.com>
2014-07-08 06:08:47 -0700

    Add comment and fixes whitespace.

--- Microsoft.VisualStudio.Composition/Export.cs
+++ Microsoft.VisualStudio.Composition/Export.cs
@@ -29,11 +29,20 @@
 
         public ExportDefinition Definition { get; private set; }
 
+        /// <summary>
+        /// Gets the metadata on the exported value.
+        /// </summary>
         public IReadOnlyDictionary<string, object> Metadata
         {
             get { return this.Definition.Metadata; }
         }
 
+        /// <summary>
+        /// Gets the exported value.
+        /// </summary>
+        /// <remarks>
+        /// This may incur a value construction cost upon first retrieval.
+        /// </remarks>
         public object Value
         {
             get { return this.exportedValueGetter(); }
@@ -43,7 +52,7 @@
         {
             Requires.NotNull(genericTypeArguments, "genericTypeArguments");
 
-            string openGenericExportTypeIdentity=(string)this.Metadata[CompositionConstants.ExportTypeIdentityMetadataName];
+            string openGenericExportTypeIdentity = (string)this.Metadata[CompositionConstants.ExportTypeIdentityMetadataName];
             string genericTypeDefinitionIdentityPattern = openGenericExportTypeIdentity;
             string[] genericTypeArgumentIdentities = genericTypeArguments.Select(ContractNameServices.GetTypeIdentity).ToArray();
             string closedTypeIdentity = string.Format(CultureInfo.InvariantCulture, genericTypeDefinitionIdentityPattern, genericTypeArgumentIdentities);
