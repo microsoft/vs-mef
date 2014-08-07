814afd624be185ac5ec36d007397fe46bef9eea2
Andrew Arnott <andarno@microsoft.com>
2014-08-07 16:45:21 -0700

    Add metadata token bit mask for future integer compression.

---
+++ Microsoft.VisualStudio.Composition/MetadataTokenType.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.VisualStudio.Composition
+{
+    /// <summary>
+    /// The bits set on metadata tokens based on type of token.
+    /// </summary>
+    /// <remarks>
+    /// This can be used in the future to remove these MSBs when serializing the metadata tokens
+    /// in order to make them more compressible by virtue of their being significantly smaller
+    /// numbers after removing the leading byte.
+    /// These come from: http://msdn.microsoft.com/en-us/library/ms231937(v=vs.110).aspx
+    /// </remarks>
+    internal enum CorTokenType
+    {
+        Type = 0x02000000,
+        Field = 0x04000000,
+        Method = 0x06000000,
+        Parameter = 0x08000000,
+        Property = 0x17000000,
+    }
+}
