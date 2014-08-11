118d53000b1f862a4aa21fe4f7f986970c13aba6
Andrew Arnott <andarno@microsoft.com>
2014-08-11 10:37:10 -0700

    Add exception serialization test.

---
+++ Microsoft.VisualStudio.Composition.Tests/CompositionFailedExceptionTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Composition;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Serialization.Formatters.Binary;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class CompositionFailedExceptionTests
+    {
+        [Fact(Skip = "Not yet implemented.")]
+        public void ExceptionIsSerializable()
+        {
+            var discovery = new AttributedPartDiscovery();
+            var catalog = ComposableCatalog.Create(new[] { discovery.CreatePart(typeof(Tree)) });
+            var configuration = CompositionConfiguration.Create(catalog);
+
+            CompositionFailedException exception = null;
+            try
+            {
+                configuration.ThrowOnErrors();
+                Assert.True(false, "Expected exception not thrown.");
+            }
+            catch (CompositionFailedException ex)
+            {
+                exception = ex;
+            }
+
+            var formatter = new BinaryFormatter();
+            var ms = new MemoryStream();
+            formatter.Serialize(ms, exception);
+
+            ms.Position = 0;
+            var actual = (CompositionFailedException)formatter.Deserialize(ms);
+            Assert.Equal(exception.Message, actual.Message);
+            Assert.NotNull(actual.Errors);
+            Assert.False(actual.Errors.IsEmpty);
+            Assert.Equal(1, actual.Errors.Peek().Count);
+            Assert.Equal(exception.Errors.Peek().Single().Message, actual.Errors.Peek().Single().Message);
+        }
+
+        [Export]
+        public class Tree
+        {
+            [Import("Fruit")] // not satisfied
+            public object Fruit { get; set; }
+        }
+    }
+}
