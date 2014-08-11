6a0acc454d75dc5dd3aa4464478424e349a0a255
Andrew Arnott <andarno@microsoft.com>
2014-08-11 14:04:05 -0700

    Add test for imports of generic type parameters that are statically available.

---
+++ Microsoft.VisualStudio.Composition.Tests/ImportOfTResolvedByDerivedClassTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    [Trait("GenericExports", "Closed")]
+    public class ImportOfTResolvedByDerivedClassTests
+    {
+        [MefFact(CompositionEngines.V1 | CompositionEngines.V2)]
+        public void ImportOfTResolvedByDerivedClass(IContainer container)
+        {
+            var derived = container.GetExportedValue<DerivedClass>();
+            Assert.NotNull(derived.ImportingProperty);
+        }
+
+        public class OpenGenericBaseClass<T>
+        {
+            [Import, MefV1.Import]
+            public T ImportingProperty { get; set; }
+        }
+
+        [Export, MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        public class DerivedClass : OpenGenericBaseClass<Apple>
+        {
+        }
+
+        [Export, MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
+        public class Apple { }
+    }
+}
