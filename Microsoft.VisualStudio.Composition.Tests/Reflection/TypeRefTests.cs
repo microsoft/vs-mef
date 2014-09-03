7ba3e6ba9d1e2bc2fdca95ab70d5712cf8fa15e8
Andrew Arnott <andarno@microsoft.com>
2014-09-03 09:49:44 -0700

    Fix TypeRef.Equals to consider IsArray property.

---
+++ Microsoft.VisualStudio.Composition.Tests/Reflection/TypeRefTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests.Reflection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.Composition.Reflection;
+    using Xunit;
+
+    public class TypeRefTests
+    {
+        [Fact]
+        public void EqualsDistinguishesArrays()
+        {
+            Assert.NotEqual(TypeRef.Get(typeof(object)), TypeRef.Get(typeof(object[])));
+        }
+    }
+}
