0b225ba24f15e7ff4e3d09845a151cbeec3559b5
Andrew Arnott <andarno@microsoft.com>
2014-10-01 07:23:34 -0700

    Add thread safety test (that fails on v1 and v3 but passes on v2).

--- Microsoft.VisualStudio.Composition.Tests/ThreadSafetyTests.cs
+++ Microsoft.VisualStudio.Composition.Tests/ThreadSafetyTests.cs
@@ -17,7 +17,7 @@
         /// to break circular dependencies in a way that tries to force
         /// thread safety issues to show themselves.
         /// </summary>
-        [MefFact(CompositionEngines.V1Compat | CompositionEngines.V2Compat)]
+        [MefFact(CompositionEngines.V1Compat | CompositionEngines.V2Compat, typeof(RootPart), typeof(SomeOtherPart))]
         public void PartRequestedAcrossMultipleThreads(IContainer container)
         {
             var testFailedCancellationSource = new CancellationTokenSource();
@@ -76,6 +76,53 @@
             }
         }
 
+        [MefFact(CompositionEngines.V1Compat | CompositionEngines.V2Compat, typeof(P1), typeof(P2))]
+        public void SharedPartNotExposedBeforeImportsAreTransitivelySatisfied(IContainer container)
+        {
+            P1.UnblockPart2Setter.Reset();
+            P1.Part2SetterInvoked.Reset();
+            var t = Task.Run(delegate
+            {
+                Task t2;
+                try
+                {
+                    P1.Part2SetterInvoked.WaitOne();
+                    t2 = Task.Run(delegate
+                    {
+                        var p2 = container.GetExportedValue<P2>();
+                        Console.WriteLine("GetExportedValue<P2> returned");
+                        var p2ViaCycle = p2.Part1.Part2;
+                        Assert.Same(p2, p2ViaCycle); // if this fails, then MEF exposed a part that imports parts that are not yet initialized.
+                    });
+
+                    // We expect this Wait to timeout because if MEF is doing the right thing,
+                    // it would block t2 from finishing until we allow P1 to finish initializing.
+                    // But that can't happen unless we give up waiting and we don't want to
+                    // deadlock when the right thing happens.
+                    Assert.False(t2.Wait(1000));
+                    Console.WriteLine("t2.Wait(int) timed out.");
+                }
+                catch (AggregateException)
+                {
+                    Console.WriteLine("t2.Wait(int) threw an exception instead of timing out.");
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine("Unblocking completion of P1.set_Part2.");
+                    P1.UnblockPart2Setter.Set();
+                }
+
+                Console.WriteLine("Getting t2 result.");
+                t2.GetAwaiter().GetResult(); // this not only propagates exceptions, but waits for completion in case of a timeout earlier.
+            });
+
+            var p1 = container.GetExportedValue<P1>();
+            Assert.Same(p1, p1.Part2.Part1);
+
+            t.GetAwaiter().GetResult();
+        }
+
         [Export]
         [MefV1.Export, MefV1.PartCreationPolicy(MefV1.CreationPolicy.NonShared)]
         public class RootPart
@@ -97,6 +144,35 @@
                 ConstructorEnteredCountdown.Signal();
                 ImportingConstructorBlockEvent.Wait(CancellationToken);
             }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class P1
+        {
+            internal static readonly ManualResetEventSlim UnblockPart2Setter = new ManualResetEventSlim();
+            internal static readonly AutoResetEvent Part2SetterInvoked = new AutoResetEvent(false);
+            private P2 part2;
+
+            [Import, MefV1.Import]
+            public P2 Part2
+            {
+                get { return this.part2; }
+                set
+                {
+                    Part2SetterInvoked.Set();
+                    UnblockPart2Setter.Wait();
+                    this.part2 = value;
+                }
+            }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class P2
+        {
+            [Import, MefV1.Import]
+            public P1 Part1 { get; set; }
         }
     }
 }
