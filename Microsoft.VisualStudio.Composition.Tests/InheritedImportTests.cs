e65947fe7f8dedce865baff6bc29546c21fb670a
Jason Malinowski <jason.malinowski@microsoft.com>
2014-10-01 19:05:46 -0700

    Add test for bug with V1 imports in a closed base type of the export.

---
+++ Microsoft.VisualStudio.Composition.Tests/InheritedImportTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    public class InheritedImportTests
+    {
+        [MefFact(CompositionEngines.V3EmulatingV1 | CompositionEngines.V3SkipCodeGenScenario, typeof(DerivedClass), typeof(ClassToImport))]
+        public void InheritedImportOnGenericBaseTypeWorks(IContainer container)
+        {
+            var derived = container.GetExportedValue<DerivedClass>();
+            Assert.IsType<DerivedClass>(derived);
+        }
+
+        [MefV1.Export]
+        public class ClassToImport { }
+
+        [MefV1.Export]
+        public class DerivedClass : BaseClass<object>
+        {
+        }
+
+        public abstract class BaseClass<T> 
+        {
+            [MefV1.Import]
+            private ClassToImport Import = null;
+        }
+    }
+}
