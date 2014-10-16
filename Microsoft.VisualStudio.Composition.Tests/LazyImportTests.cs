a4b9d447b71dd4738a79387814849766698db45b
Andrew Arnott <andarno@microsoft.com>
2014-10-16 14:14:33 -0700

    Fix deep initialization of MEF parts.

--- Microsoft.VisualStudio.Composition.Tests/LazyImportTests.cs
+++ Microsoft.VisualStudio.Composition.Tests/LazyImportTests.cs
@@ -7,6 +7,7 @@
     using System.Text;
     using System.Threading.Tasks;
     using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
 
     public class LazyImportTests
     {
@@ -84,8 +85,118 @@
 
         #endregion
 
+        #region Lazy entrypoint to a non-lazy chain of imports
+
+        /// <summary>
+        /// Verifies that imports are satisfied deeply.
+        /// </summary>
+        /// <remarks>
+        /// This may seem arbitrary but when this test was written, there was a bug that only manifested
+        /// after the sixth link. I added several more for durability of the test.
+        /// </remarks>
+        [MefFact(CompositionEngines.V1Compat | CompositionEngines.V2Compat, typeof(Link1), typeof(Link2), typeof(Link3), typeof(Link4), typeof(Link5), typeof(Link6), typeof(Link7), typeof(Link8), typeof(Link9), typeof(AnotherExport))]
+        public void LazyEntrypointToNonLazyChain(IContainer container)
+        {
+            Link2.CtorInvocationCounter = 0;
+            var chain = container.GetExportedValue<Link1>();
+            Assert.NotNull(chain.Link);
+            Assert.Equal(0, Link2.CtorInvocationCounter);
+            Assert.NotNull(chain.Link.Value);
+            Assert.Equal(1, Link2.CtorInvocationCounter);
+            Assert.NotNull(chain.Link.Value.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link.Link.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link.Link.Link.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link.Link.Link.Link.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link.Link.Link.Link.Link.Link);
+            Assert.NotNull(chain.Link.Value.Link.Link.Link.Link.Link.Link.Link.Link);
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link1
+        {
+            [Import, MefV1.Import]
+            public Lazy<Link2> Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link2
+        {
+            internal static int CtorInvocationCounter;
+
+            public Link2()
+            {
+                CtorInvocationCounter++;
+            }
+
+            [Import, MefV1.Import]
+            public Link3 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link3
+        {
+            [Import, MefV1.Import]
+            public Link4 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link4
+        {
+            [Import, MefV1.Import]
+            public Link5 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link5
+        {
+            [Import, MefV1.Import]
+            public Link6 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link6
+        {
+            [Import, MefV1.Import]
+            public Link7 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link7
+        {
+            [Import, MefV1.Import]
+            public Link8 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link8
+        {
+            [Import, MefV1.Import]
+            public Link9 Link { get; set; }
+        }
+
+        [Export, Shared]
+        [MefV1.Export]
+        public class Link9
+        {
+            [Import, MefV1.Import]
+            public AnotherExport Link { get; set; }
+        }
+
+        #endregion
+
         [Export]
         [Export("AnotherExport", typeof(object))]
+        [MefV1.Export]
         public class AnotherExport
         {
             internal static int ConstructionCount;
