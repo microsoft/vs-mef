36eec9cd3c12ad9d2b5629a4f946fb3340043b1a
Andrew Arnott <andarno@microsoft.com>
2014-11-12 17:05:30 -0800

    Perf: Avoid Linq in constructing Type[]

--- Microsoft.VisualStudio.Composition/ReflectionHelpers.cs
+++ Microsoft.VisualStudio.Composition/ReflectionHelpers.cs
@@ -413,6 +413,20 @@
         /// <returns>A constructed type.</returns>
         internal static Type CloseGenericType(Type genericTypeDefinition, Type constructedType)
         {
+            using (var typeArguments = ExtractGenericTypeArguments(genericTypeDefinition, constructedType))
+            {
+                return genericTypeDefinition.MakeGenericType(typeArguments.Value);
+            }
+        }
+
+        /// <summary>
+        /// Extracts generic type arguments from a constructed type that are necessary to close a generic type definition.
+        /// </summary>
+        /// <param name="genericTypeDefinition">A generic type definition.</param>
+        /// <param name="constructedType">A closed type from which may be obtained generic type arguments.</param>
+        /// <returns>The type argument necessary to construct the closed type.</returns>
+        internal static Rental<Type[]> ExtractGenericTypeArguments(Type genericTypeDefinition, Type constructedType)
+        {
             Requires.NotNull(genericTypeDefinition, "genericTypeDefinition");
             Requires.NotNull(constructedType, "constructedType");
 
@@ -427,7 +441,13 @@
 
             Requires.Argument(constructedGenericType != null, "constructedType", "Not a closed form of the other.");
 
-            return genericTypeDefinition.MakeGenericType(constructedGenericType.GenericTypeArguments.Take(genericTypeDefinitionInfo.GenericTypeParameters.Length).ToArray());
+            var result = ArrayRental<Type>.Get(genericTypeDefinitionInfo.GenericTypeParameters.Length);
+            for (int i = 0; i < result.Value.Length; i++)
+            {
+                result.Value[i] = constructedGenericType.GenericTypeArguments[i];
+            }
+
+            return result;
         }
 
         internal static Type GetExportedValueType(Type declaringType, MemberInfo exportingMember)
