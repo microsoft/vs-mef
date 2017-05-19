72562809098415f8b13ba65a7776956d69898835
Kirill Osenkov <github@osenkov.com>
2017-05-19 13:01:01 -0700

    Fixes https://github.com/Microsoft/vs-mef/issues/7.
    If xUnit asserts fail in another appdomain, instead of the legible error we get
    Type 'Xunit.Sdk.FalseException' in assembly 'xunit.assert, Version=2.0.0.2929, Culture=neutral, PublicKeyToken=8d05b1bb7a6fdb6c' is not marked as serializable.
    
    Instead, we have our own serializable exception that can travel across appdomains and preserve a better error message in case any of these tests fail.

---
+++ src/tests/Microsoft.VisualStudio.Composition.Tests/XunitExtensions/AssertFailedException.cs
@@ -0,0 +1,28 @@
+﻿namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    internal class AssertFailedException : Exception
+    {
+        public AssertFailedException()
+        {
+        }
+
+        public AssertFailedException(string message)
+            : base(message)
+        {
+        }
+
+        public AssertFailedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected AssertFailedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
