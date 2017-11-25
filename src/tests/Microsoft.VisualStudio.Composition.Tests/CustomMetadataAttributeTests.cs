7447e8922f82639b17cd75cc9a7afa09b9edfad7
Andrew Arnott <andarno@microsoft.com>
2017-11-25 07:38:21 -0800

    Move original test into its own file so we can build on it with many more tests
    And the custom metadata attribute tests class wasn't appropriate because the issue that was being tested was metadata view handling -- the fact that the metadata view class was also a custom metadata attribute is purely incidental to VS MEF.

--- src/tests/Microsoft.VisualStudio.Composition.Tests/CustomMetadataAttributeTests.cs
+++ src/tests/Microsoft.VisualStudio.Composition.Tests/CustomMetadataAttributeTests.cs
@@ -157,45 +157,6 @@ namespace Microsoft.VisualStudio.Composition.Tests
             Assert.Empty(catalog.DiscoveredParts.DiscoveryErrors);
         }
 
-        /* Tests to add:
-         *   internal constructor on metadata view?
-         *   internal properties on metadata view?
-         *   metadata for which there is no matching properties?
-         *   defaultValues?
-         *   case sensitivity?
-         *   exceptions thrown from setter?
-         *   no default ctor, but has a constructor that takes some parameters.
-         *   test properties without setters.
-         */
-
-        [MefFact(CompositionEngines.V2Compat, typeof(PartWithStronglyTypedMetadata), typeof(PartContainingPartWithStronglyTypedMetadata))]
-        public void StronglyTypedMetadataOnType(IContainer container)
-        {
-            var part = container.GetExportedValue<PartContainingPartWithStronglyTypedMetadata>();
-            Assert.NotNull(part);
-            Assert.NotNull(part.InnerPart);
-            Assert.Equal("MetadataPropertyValue", part.InnerPart.Metadata.Property);
-        }
-
-        [Export]
-        public class PartContainingPartWithStronglyTypedMetadata
-        {
-            [Import]
-            public Lazy<PartWithStronglyTypedMetadata, StronglyTypedMetadata> InnerPart { get; set; }
-        }
-
-        [Export]
-        [StronglyTypedMetadata(Property = "MetadataPropertyValue")]
-        public class PartWithStronglyTypedMetadata
-        {
-        }
-
-        [MetadataAttribute]
-        public class StronglyTypedMetadata : Attribute
-        {
-            public string Property { get; set; }
-        }
-
         [MefV1.Export, Export]
         [DerivedMetadata("prop1", "prop2")]
         public class ExportWithDerivedExportMetadata
---
+++ src/tests/Microsoft.VisualStudio.Composition.Tests/MetadataViewClassDefaultCtorProviderTests.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.VisualStudio.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Composition;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xunit;
+    using MefV1 = System.ComponentModel.Composition;
+
+    public class MetadataViewClassDefaultCtorProviderTests
+    {
+        /* Tests to add:
+         *   internal constructor on metadata view?
+         *   internal properties on metadata view?
+         *   metadata for which there is no matching properties?
+         *   defaultValues?
+         *   case sensitivity?
+         *   exceptions thrown from setter?
+         *   no default ctor, but has a constructor that takes some parameters.
+         *   test properties without setters.
+         */
+
+        [MefFact(CompositionEngines.V2Compat, typeof(PartWithStronglyTypedMetadata), typeof(PartContainingPartWithStronglyTypedMetadata))]
+        public void StronglyTypedMetadataOnType(IContainer container)
+        {
+            var part = container.GetExportedValue<PartContainingPartWithStronglyTypedMetadata>();
+            Assert.NotNull(part);
+            Assert.NotNull(part.InnerPart);
+            Assert.Equal("MetadataPropertyValue", part.InnerPart.Metadata.Property);
+        }
+
+        [Export]
+        public class PartContainingPartWithStronglyTypedMetadata
+        {
+            [Import]
+            public Lazy<PartWithStronglyTypedMetadata, StronglyTypedMetadata> InnerPart { get; set; }
+        }
+
+        [Export]
+        [StronglyTypedMetadata(Property = "MetadataPropertyValue")]
+        public class PartWithStronglyTypedMetadata
+        {
+        }
+
+        [MetadataAttribute]
+        public class StronglyTypedMetadata : Attribute
+        {
+            public string Property { get; set; }
+        }
+    }
+}
