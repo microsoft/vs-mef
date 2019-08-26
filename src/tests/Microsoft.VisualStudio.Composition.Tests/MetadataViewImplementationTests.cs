e2fa0cc7f8fe4fa0400400dc7725a9808e05ade8
Andrew Arnott <andarno@microsoft.com>
2019-08-26 09:22:10 -0600

    Verify behavior when mismatching export metadata is not included

--- src/tests/Microsoft.VisualStudio.Composition.Tests/MetadataViewImplementationTests.cs
+++ src/tests/Microsoft.VisualStudio.Composition.Tests/MetadataViewImplementationTests.cs
@@ -5,15 +5,22 @@ namespace Microsoft.VisualStudio.Composition.Tests
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Xunit;
     using MefV1 = System.ComponentModel.Composition;
 
     public class MetadataViewImplementationTests
     {
-        [MefFact(CompositionEngines.V1Compat)]
+        [MefFact(CompositionEngines.V1Compat, typeof(ExportingPartWithoutMismatchingMetadata))]
+        public void MetadataViewImplementationDirectQuery_WithoutMismatchingMetadata(IContainer container)
+        {
+            var export = container.GetExport<ExportingPartWithoutMismatchingMetadata, IMetadataView>();
+            Assert.IsType<MetadataViewClass>(export.Metadata);
+            Assert.Equal("1", export.Metadata.A);
+            Assert.Null(export.Metadata.B);
+            Assert.Equal(20, export.Metadata.PropertyWithMetadataThatDoesNotMatchType);
+        }
+
+        [MefFact(CompositionEngines.V1Compat, typeof(ImportingPart), typeof(ExportingPart))]
         public void MetadataViewImplementationDirectQuery(IContainer container)
         {
             var export = container.GetExport<ExportingPart, IMetadataView>();
@@ -23,7 +30,17 @@ namespace Microsoft.VisualStudio.Composition.Tests
             Assert.Equal(10, export.Metadata.PropertyWithMetadataThatDoesNotMatchType);
         }
 
-        [MefFact(CompositionEngines.V1Compat)]
+        [MefFact(CompositionEngines.V1Compat, typeof(ImportingPartWithoutMismatchingMetadata), typeof(ExportingPartWithoutMismatchingMetadata))]
+        public void MetadataViewImplementationViaImport_WithoutMismatchingMetadata(IContainer container)
+        {
+            var importingPart = container.GetExportedValue<ImportingPartWithoutMismatchingMetadata>();
+            Assert.IsType<MetadataViewClass>(importingPart.ImportingProperty.Metadata);
+            Assert.Equal("1", importingPart.ImportingProperty.Metadata.A);
+            Assert.Null(importingPart.ImportingProperty.Metadata.B);
+            Assert.Equal(20, importingPart.ImportingProperty.Metadata.PropertyWithMetadataThatDoesNotMatchType);
+        }
+
+        [MefFact(CompositionEngines.V1Compat, typeof(ImportingPart), typeof(ExportingPart))]
         public void MetadataViewImplementationViaImport(IContainer container)
         {
             var importingPart = container.GetExportedValue<ImportingPart>();
@@ -31,6 +48,13 @@ namespace Microsoft.VisualStudio.Composition.Tests
             Assert.Equal("1", importingPart.ImportingProperty.Metadata.A);
             Assert.Null(importingPart.ImportingProperty.Metadata.B);
             Assert.Equal(10, importingPart.ImportingProperty.Metadata.PropertyWithMetadataThatDoesNotMatchType);
+        }
+
+        [MefV1.Export]
+        public class ImportingPartWithoutMismatchingMetadata
+        {
+            [MefV1.Import]
+            public Lazy<ExportingPartWithoutMismatchingMetadata, IMetadataView> ImportingProperty { get; set; }
         }
 
         [MefV1.Export]
@@ -44,6 +68,10 @@ namespace Microsoft.VisualStudio.Composition.Tests
         [MefV1.ExportMetadata("A", "1")]
         [MefV1.ExportMetadata("PropertyWithMetadataThatDoesNotMatchType", "valueThatDoesNotMatchType")]
         public class ExportingPart { }
+
+        [MefV1.Export]
+        [MefV1.ExportMetadata("A", "1")]
+        public class ExportingPartWithoutMismatchingMetadata { }
 
         [MefV1.MetadataViewImplementation(typeof(MetadataViewClass))]
         public interface IMetadataView
