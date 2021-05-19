83860860e28510bcb905b354ceee54c7cb067233
Miguel Lopez <milopezc@microsoft.com>
2021-05-19 16:35:23 -0700

    Migrate to LCL localization

--- init.ps1
+++ init.ps1
@@ -3,12 +3,19 @@
     Restores NuGet packages.
 .PARAMETER Signing
     Install the MicroBuild signing plugin for building test-signed builds on desktop machines.
+.PARAMETER Localization
+    Install the MicroBuild localization plugin for building loc builds on desktop machines.
+    The environment is configured to build pseudo-loc for JPN only, but may be used to build
+    all languages with shipping-style loc by using the `/p:loctype=full,loclanguages=vs`
+    when building.
 .PARAMETER IBCMerge
     Install the MicroBuild IBCMerge plugin for building optimized assemblies on desktop machines.
 #>
 Param(
     [Parameter()]
     [switch]$Signing,
+    [Parameter()]
+    [switch]$Localization,
     [Parameter()]
     [switch]$IBCMerge
 )
@@ -30,6 +37,13 @@ try {
         Write-Host "Installing MicroBuild signing plugin" -ForegroundColor $HeaderColor
         & "$toolsPath\Install-NuGetPackage.ps1" MicroBuild.Plugins.Signing -source $MicroBuildPackageSource -Verbosity $nugetVerbosity
         $env:SignType = "Test"
+    }
+
+    if ($Localization) {
+        Write-Host "Installing MicroBuild localization plugin" -ForegroundColor $HeaderColor
+        & $InstallNuGetPkgScriptPath MicroBuild.Plugins.Localization -source $MicroBuildPackageSource -Verbosity $nugetVerbosity
+        $EnvVars['LocType'] = "Pseudo"
+        $EnvVars['LocLanguages'] = "JPN"
     }
 
     if ($IBCMerge) {
