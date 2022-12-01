e71cc9f2441930a902fc6a49d77225ac388da24f
Andrew Arnott <andrew.arnott@microsoft.com>
2022-12-01 14:26:55 -0700

    Drop code coverage
    I think it's the cause of our test runs to hanging.

--- azure-pipelines/dotnet-test-cloud.ps1
+++ azure-pipelines/dotnet-test-cloud.ps1
@@ -48,7 +48,6 @@ if ($x86) {
     --no-build `
     -c $Configuration `
     --filter "TestCategory!=FailsInCloudTest" `
-    --collect "Code Coverage;Format=cobertura" `
     --settings "$PSScriptRoot/test.runsettings" `
     --blame-hang-timeout 60s `
     --blame-crash `
