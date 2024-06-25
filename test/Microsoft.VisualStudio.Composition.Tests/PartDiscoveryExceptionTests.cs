26b5367070fb8869b9b75ce15f18a1d518da2721
Andrew Arnott <andrew.arnott@microsoft.com>
2024-06-25 14:59:59 -0600

    Bring back deleted test

--- test/Microsoft.VisualStudio.Composition.Tests/PartDiscoveryExceptionTests.cs
+++ test/Microsoft.VisualStudio.Composition.Tests/PartDiscoveryExceptionTests.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.VisualStudio.Composition.Tests
@@ -10,6 +10,24 @@ namespace Microsoft.VisualStudio.Composition.Tests
 
     public class PartDiscoveryExceptionTests
     {
+#if !NETCOREAPP
+        [Fact]
+        public void ExceptionIsSerializable_BinaryFormatter()
+        {
+            var exception = new PartDiscoveryException("msg") { AssemblyPath = "/some path", ScannedType = typeof(string) };
+
+            var formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            var ms = new MemoryStream();
+            formatter.Serialize(ms, exception);
+
+            ms.Position = 0;
+            var actual = (PartDiscoveryException)formatter.Deserialize(ms);
+            Assert.Equal(exception!.Message, actual.Message);
+            Assert.Equal(exception.ScannedType, actual.ScannedType);
+            Assert.Equal(exception.AssemblyPath, actual.AssemblyPath);
+        }
+#endif
+
         [Fact]
         public void ExceptionIsSerializable()
         {
