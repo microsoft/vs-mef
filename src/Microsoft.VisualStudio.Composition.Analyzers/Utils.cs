88c972956cd4064317629aa8c5e6ef4684c5f8da
Drew Noakes <git@drewnoakes.com>
2025-07-28 20:53:24 +1000

    Use file-scoped namespace

--- src/Microsoft.VisualStudio.Composition.Analyzers/Utils.cs
+++ src/Microsoft.VisualStudio.Composition.Analyzers/Utils.cs
@@ -1,26 +1,20 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.VisualStudio.Composition.Analyzers
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-    using Microsoft.CodeAnalysis.Diagnostics;
+namespace Microsoft.VisualStudio.Composition.Analyzers;
 
+/// <summary>
+/// Utility methods for analyzers.
+/// </summary>
+internal static class Utils
+{
     /// <summary>
-    /// Utility methods for analyzers.
+    /// Gets the URL to the help topic for a particular analyzer.
     /// </summary>
-    internal static class Utils
+    /// <param name="analyzerId">The ID of the analyzer.</param>
+    /// <returns>The URL for the analyzer's documentation.</returns>
+    internal static string GetHelpLink(string analyzerId)
     {
-        /// <summary>
-        /// Gets the URL to the help topic for a particular analyzer.
-        /// </summary>
-        /// <param name="analyzerId">The ID of the analyzer.</param>
-        /// <returns>The URL for the analyzer's documentation.</returns>
-        internal static string GetHelpLink(string analyzerId)
-        {
-            return $"https://github.com/Microsoft/vs-mef/blob/main/doc/analyzers/{analyzerId}.md";
-        }
+        return $"https://github.com/Microsoft/vs-mef/blob/main/doc/analyzers/{analyzerId}.md";
     }
 }
